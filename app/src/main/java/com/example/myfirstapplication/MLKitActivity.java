aae75b23441d479835245fd8c6517ec51594d57e
Finn O'Brien <Finnob10@gmail.com>
2026-04-01 15:15:19 +1100

    week 7 commit

--- app/src/main/java/com/example/myfirstapplication/MLKitActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MLKitActivity.java
@@ -1,13 +1,19 @@
 package com.example.myfirstapplication;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -34,6 +40,8 @@ import com.google.mlkit.vision.barcode.common.Barcode;
 import com.google.mlkit.vision.common.InputImage;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class MLkitActivity extends AppCompatActivity {
@@ -56,6 +64,13 @@ public class MLkitActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.imageViewMLKit);
         textViewOutput = findViewById(R.id.textViewMLKit);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String result = extras.getString("result");
+            textViewOutput.setText(result);
+            Uri uri = Uri.parse(extras.getString("uri"));
+            imageView.setImageURI(uri);
+        }
     }
 
     private boolean checkPermission() {
@@ -83,7 +98,59 @@ public class MLkitActivity extends AppCompatActivity {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         activityResultLauncher.launch(galleryIntent);
+
     }
+
+    public void openListView(View view) {
+// Get image from the current imageFileUri
+        Bitmap bitmap = getBitmapFromUri(imageFileUri);
+// Create a unique filename from the current date time
+        String currentDateTime = LocalDateTime.now().toString();
+        String imageFilename = currentDateTime.replaceAll("\\D+", "");
+// Save the image to gallery with the unique filename
+        saveImageToGallery(bitmap, imageFilename, MLkitActivity.this);
+// open List View activity
+        Intent intent = new Intent(MLkitActivity.this, ListViewActivity.class);
+        intent.putExtra("reader", "Barcode Reader");
+        intent.putExtra("result", textViewOutput.getText().toString());
+        intent.putExtra("filename", imageFilename);
+        startActivity(intent);
+    }
+
+
+    private Bitmap getBitmapFromUri(Uri uri) {
+        try {
+            ImageDecoder.Source source =
+                    ImageDecoder.createSource(getContentResolver(), uri);
+            Bitmap bitmap = ImageDecoder.decodeBitmap(source);
+            return bitmap;
+        } catch (IOException e) {
+            Log.e("URI_TO_BITMAP", "Failed to load image", e);
+            return null;
+        }
+    }
+    private void saveImageToGallery(Bitmap bitmap, String fileName, Context
+            context) {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, fileName);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png");
+        values.put(MediaStore.Images.Media.RELATIVE_PATH,
+                Environment.DIRECTORY_PICTURES);
+        Uri imageUri =
+                context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        try {
+            OutputStream outputStream =
+                    context.getContentResolver().openOutputStream(imageUri);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            Log.d("SAVE_GALLERY", "Image saved to gallery: " +
+                    imageUri.toString());
+        } catch (IOException e) {
+            Log.e("SAVE_GALLERY", "Error saving image", e);
+        }
+    }
+
 
 
     ActivityResultLauncher<Intent> activityResultLauncher =
